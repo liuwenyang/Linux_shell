c008d29f1f25d57489fdcb1bee5876d814df5edb
liuwenyang <1843829634@qq.com>
2024-03-30 03:01:40 +0800

    Automated commit on 2024-03-30 03:01:40

--- install_software.sh
+++ install_software.sh
@@ -5,12 +5,12 @@ check_and_install() {
     package=$1
     if ! dpkg -l | grep -qw $package; then
         echo "$package 未安装，正在安装..."
+        echo
         sudo apt-get install -y $package
     else
         echo "$package 已安装，跳过..."
     fi
 }
-
 
 #使用apt安装的软件
 # 更新软件包信息
@@ -18,7 +18,7 @@ echo "正在更新软件包信息..."
 sudo apt-get update -y
 
 # 定义需要检查的软件列表
-packages=("vim" "tree" "wget" "netcat" "lsof" "tcpdump" "ntpdate" "openssh-server" "at" "htop" "curl" "telnet" "vlc" "hardinfo" )
+packages=("vim" "tree" "wget" "netcat" "lsof" "tcpdump" "ntpdate" "openssh-server" "at" "htop" "curl" "telnet" "vlc" "hardinfo")
 
 # 遍历软件列表，检查并安装
 for package in "${packages[@]}"; do
@@ -28,7 +28,11 @@ done
 # 打印安装完成的软件列表
 echo "所有请求安装的软件处理完成，列表如下："
 for package in "${packages[@]}"; do
-    echo $package
+    if ! dpkg -l | grep -qw $package; then
+        echo "$package 未安装"
+    else
+        echo "$package 已安装"
+    fi
 done
 
 #特殊情况安装包
