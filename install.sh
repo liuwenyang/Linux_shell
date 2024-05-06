fc7c7aff7e95eac57a6025c2ade7fcb3adc0e768
Silence <1843829634@qq.com>
2024-05-06 14:41:46 +0800

    解决判断参数未执行清理脚本的语法问题

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ bash $script_path/run_alias_car.sh
 
 bash $script_path/run_clearzcfiles.sh
 
-if [ $1 != usb];then
+if [ "$1" != "usb" ]; then
     bash $script_path/clean_trash.sh 
     echo 看看我清理文件执行了吗
 fi
