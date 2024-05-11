fa6af65e6ad048357940a7b8dbf4c77faf5192cf
Silence <1843829634@qq.com>
2024-05-11 14:07:30 +0800

    每5分钟ntpdate  造成bug 到期执行一次, 系统时间超过crontab计时时间 造成循环 时间就会不停叠加8小时  !!改为大于8小时1校时

--- config_crontab.sh
+++ config_crontab.sh
@@ -21,8 +21,10 @@ safe_save_cron="0 4 * * 1 /home/storage/config/yaml/safe_save.sh"
 safe_save_yaml="0 * * * * cp /home/storage/config/yaml/config.yaml /home/storage/config/yaml/config.yaml_1h_AutoSave"
 #同步系统时间到硬件时间;将硬件时钟调整为与目前的系统时钟一致，每天0点
 hwclock_cron="0 0 * * * timedatectl set-local-rtc 0 && hwclock -w && hwclock --systohc"
-#每5分钟ntpdate
-update_time="*/5 * * * * /home/storage/zc/update_time.sh"
+#由于 cron 的工作原理是根据系统时间来调度任务，每次脚本运行后系统时间的这种突增会影响到 cron 的下一次执行计划。
+
+#具体来说，cron 使用系统时间来决定何时执行计划中的任务。如果你的脚本将系统时间快速推进8小时，那么实际上在短时间内系统时间远远超过了原本应该按5分钟间隔进行的计划。例如，如果脚本在某个点执行并将时间从13:00推进到21:00，那么所有在这个时间段内应该执行的 cron 任务（每5分钟一次）都会被触发执行，因为系统认为这些任务的执行时间已经到了
+update_time="0 */12 * * * /home/storage/zc/update_time.sh" #每5分钟ntpdate  造成bug 到期执行一次, 系统时间超过crontab计时时间 造成循环 时间就会不停叠加8小时  !!改为大于8小时1校时
 ######定义cron任务区结束######
 
 
