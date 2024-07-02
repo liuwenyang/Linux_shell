e9cd1e72cc34f3106f1fe9cc6fc7f1cd633a57a6
Silence <1843829634@qq.com>
2024-07-02 14:59:08 +0800

    增加点位快捷命令

--- alias_train.sh
+++ alias_train.sh
@@ -12,8 +12,8 @@ alias cz='cd /home/storage/matrixai/cz/project'
 alias log-c='logc > $docker_log_path/$(now)_client.log && cd $docker_log_path && ls'
 alias log-m='logm > $docker_log_path/$(now)_server.log && cd $docker_log_path && ls'
 alias up-y='cp /home/storage/config/yaml/config.yaml /home/storage/config/yaml/config.yaml_$(now) && mv /tmp/config.yaml /home/storage/zc/ ; mv /home/storage/zc/config.yaml /home/storage/config/yaml ; chmod 777 /home/storage/config/yaml/config.yaml && rs-c'
-
-
+alias 0209='echo -n "8D08200209000000" | xxd -r -p | nc localhost 6669 && echo 读写参数完成'
+alias 1402='echo -n "8D08201402000000" | xxd -r -p | nc localhost 6669 && echo 重读车号完成'
 
 #火车组定制化
 alias logc="docker logs -f --tail 1000 cli" 
