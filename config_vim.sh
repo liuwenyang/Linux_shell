625b6a9f3cc3bbf15956b4e387a9790e575868a8
Silence <1843829634@qq.com>
2025-09-15 21:44:19 +0800

    去除w2u vim配置修改

--- config_vim.sh
+++ config_vim.sh
@@ -3,13 +3,25 @@
 
 #带''是为了防止打印字符转义
 cat << 'EOF' >> ~/.vimrc
-set number "显示行号"
-set ruler"显示当前光标位置"
+#设置显示行号
+set number
+
+#启用语法高亮
+syntax on
+
+#在底部显示当前光标位置
+set ruler
+
+#其他一些常用配置...
+#设置Tab键为4个空格
+set tabstop=4
+set shiftwidth=4
+set expandtab
 
 EOF
 
 
-#echo 请手动执行如下命令
-#echo "source ~/.vimrc" 
+echo 请手动执行如下命令
+echo "source ~/.vimrc" 
 #source /root/.vimrc && echo ".vimrc修改完成"
 
